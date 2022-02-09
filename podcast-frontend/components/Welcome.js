6c28d95d60ad29ef447dd202f925b685dc55be7b
Atharva <atharvadeshpande174@gmail.com>
2022-02-09 13:29:05 +0530

    Added Landing Page

---
+++ podcast-frontend/components/Welcome.js
@@ -0,0 +1,16 @@
+import { Button } from '@material-ui/core';
+import Link from 'next/link';
+import React from 'react';
+import { signIn, signOut, useSession } from "next-auth/react";
+function Welcome() {
+  return <div className = "text-white flex-1 grid place-content-center items-center  w-full  bg-black overflow-y-scroll scrollbar-thin scrollbar-thumb-black">
+      <p className='text-white text-4xl text-center font-bold mb-1'>Welcome to PodFast</p>
+      <p className='text-white text-1xl text-center mb-1'>Login to get started...</p>
+      <Link href = "/login">
+            <Button variant='contained' color = 'secondary'>Login</Button>
+  
+      </Link>
+      </div>;
+}
+
+export default Welcome;
