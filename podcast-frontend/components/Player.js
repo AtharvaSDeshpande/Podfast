a4186d3cacbbc9e0fc9361ed995db30751d05743
Atharva <atharvadeshpande174@gmail.com>
2022-02-19 13:05:42 +0530

    Added Dashboard and removed player bug

--- podcast-frontend/components/Player.js
+++ podcast-frontend/components/Player.js
@@ -23,15 +23,15 @@ function Player() {
     const animationRef = useRef();  // reference the animation
 
     useEffect(() => {
-        const seconds = Math.floor(audioPlayer.current.duration);
+        const seconds = Math?.floor(audioPlayer?.current?.duration);
         setDuration(seconds);
-        progressBar.current.max = seconds;
+        progressBar?.current?.max = seconds;
     }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
 
     const calculateTime = (secs) => {
-        const minutes = Math.floor(secs / 60);
+        const minutes = Math?.floor(secs / 60);
         const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-        const seconds = Math.floor(secs % 60);
+        const seconds = Math?.floor(secs % 60);
         const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
         return `${returnedMinutes}:${returnedSeconds}`;
     }
@@ -40,53 +40,53 @@ function Player() {
         const prevValue = isPlaying;
         setIsPlaying(!prevValue);
         if (!prevValue) {
-            audioPlayer.current.play();
-            animationRef.current = requestAnimationFrame(whilePlaying)
+            audioPlayer?.current?.play();
+            animationRef?.current = requestAnimationFrame(whilePlaying)
         } else {
-            audioPlayer.current.pause();
-            cancelAnimationFrame(animationRef.current);
+            audioPlayer?.current?.pause();
+            cancelAnimationFrame(animationRef?.current);
         }
     }
 
     const whilePlaying = () => {
-        progressBar.current.value = audioPlayer.current.currentTime;
+        progressBar?.current?.value = audioPlayer?.current?.currentTime;
         changePlayerCurrentTime();
-        animationRef.current = requestAnimationFrame(whilePlaying);
+        animationRef?.current = requestAnimationFrame(whilePlaying);
     }
 
     const changeRange = () => {
-        audioPlayer.current.currentTime = progressBar.current.value;
+        audioPlayer?.current?.currentTime = progressBar?.current?.value;
         changePlayerCurrentTime();
     }
 
     const changePlayerCurrentTime = () => {
-        progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
-        setCurrentTime(progressBar.current.value);
+        progressBar?.current?.style?.setProperty('--seek-before-width', `${progressBar?.current?.value / duration * 100}%`)
+        setCurrentTime(progressBar?.current?.value);
     }
     const updateCurrentTime = () => {
-        setCurrentTime(progressBar.current.value);
+        setCurrentTime(progressBar?.current?.value);
     };
     const changeAudioToPlayhead = () => {
-        audioPlayer.current.currentTime = progressBar.current.value;
-        setCurrentTime(progressBar.current.value);
-        progressBar.current.style.setProperty(
+        audioPlayer?.current?.currentTime = progressBar?.current?.value;
+        setCurrentTime(progressBar?.current?.value);
+        progressBar?.current?.style?.setProperty(
             '--seek-before-width',
-            `${(progressBar.current.value / duration) * 100}%`
+            `${(progressBar?.current?.value / duration) * 100}%`
         );
     };
 
     const timeTravel = (newTime) => {
-        progressBar.current.value = newTime;
+        progressBar?.current?.value = newTime;
         updateCurrentTime();
         changeAudioToPlayhead();
     };
 
     const backThirty = () => {
-        timeTravel(Number(progressBar.current.value) - 30);
+        timeTravel(Number(progressBar?.current?.value) - 30);
     }
 
     const forwardThirty = () => {
-        timeTravel(Number(progressBar.current.value) + 30);
+        timeTravel(Number(progressBar?.current?.value) + 30);
     }
 
     const [vol, setVol] = useState(50);
--- podcast-frontend/pages/dashboard.js
+++ podcast-frontend/pages/dashboard.js
@@ -1,20 +1,21 @@
 import Head from 'next/head'
+import Header from '../components/Dashboard/Header';
 import { isCreator, isSignedIn, loginUser } from '../components/functions';
-import Header from '../components/Header';
+
 import Welcome from '../components/Welcome';
 import { useStateValue } from '../redux/StateProvider'
 
 
+const Dashboard =  () => {
 
-export default function Dashboard() {
     const [{ user }, dispatch] = useStateValue();
-    if (user == null)
-    {
-      loginUser();
+    if (user == null) {
+        loginUser();
     }
+
     if (!isSignedIn(user))
-        return (<Welcome />)
-    if (!isCreator(user))
+        return <Welcome/>
+    else if (!isCreator(user))
         return (
             <>
                 <Head>
@@ -35,14 +36,37 @@ export default function Dashboard() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header/>
-            <main className="flex flex-col items-center justify-center min-h-screen  bg-neutral-900 h-screen overflow-y-scroll ">
-                <div className='text-white'>
-                  Welcome to Creator Dashboard
-                </div>
-
-            </main>
-
-
+            
         </div>
     )
 }
+export default Dashboard ;
+
+
+{/* <ul >
+                            <li className="mr-6 my-2 md:my-0">
+                                <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-blue-400 no-underline hover:text-gray-100 border-b-2 border-blue-400 hover:border-blue-400">
+                                    <Home className="fas fa-home fa-fw mr-3 text-blue-400" /><span className="pb-1 md:pb-0 text-sm">Home</span>
+                                </a>
+                            </li>
+                            <li className="mr-6 my-2 md:my-0">
+                                <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-100 border-b-2 border-gray-900  hover:border-pink-400">
+                                    <TodayOutlined className="fas fa-tasks fa-fw mr-3" /><span className="pb-1 md:pb-0 text-sm">Tasks</span>
+                                </a>
+                            </li>
+                            <li className="mr-6 my-2 md:my-0">
+                                <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-100 border-b-2 border-gray-900  hover:border-purple-400">
+                                    <Message className="fa fa-envelope fa-fw mr-3" /><span className="pb-1 md:pb-0 text-sm">Messages</span>
+                                </a>
+                            </li>
+                            <li className="mr-6 my-2 md:my-0">
+                                <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-100 border-b-2 border-gray-900  hover:border-green-400">
+                                    <GraphicEq className="fas fa-chart-area fa-fw mr-3" /><span className="pb-1 md:pb-0 text-sm">Analytics</span>
+                                </a>
+                            </li>
+                            <li className="mr-6 my-2 md:my-0">
+                                <a href="#" className="block py-1 md:py-3 pl-1 align-middle text-gray-500 no-underline hover:text-gray-100 border-b-2 border-gray-900  hover:border-red-400">
+                                    <Payment className="fa fa-wallet fa-fw mr-3" /><span className="pb-1 md:pb-0 text-sm">Payments</span>
+                                </a>
+                            </li>
+                        </ul> */}
