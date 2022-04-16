ab673eb78f8113eaa261952401f17922270d089d
sshrutii <shrutirahul2000@gmail.com>
2022-04-16 23:46:34 +0530

    mongo

--- Backend/precommender/settings.py
+++ Backend/precommender/settings.py
@@ -74,15 +74,25 @@ WSGI_APPLICATION = 'precommender.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'PodFastDatabase',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host':'mongodb+srv://atharvasd14:6IoJbs4HD0uj7JiE@podfast.prsoq.mongodb.net/PodFastDatabase?retryWrites=true&w=majority'
+        }  
+    }
+}
+   
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
