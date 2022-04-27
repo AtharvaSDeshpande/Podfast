8c45dce722c9b53191184021b89b649125a213a6
mehakkkk <mehakgupta160@gmail.com>
2022-04-27 09:22:11 +0530

    comment-updated

--- Backend/growth_predict/views.py
+++ Backend/growth_predict/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from .data_management import DataManager
 from django.http import HttpResponse
-from django.http import JsonResponse
+from utils import *
+from bson.objectid import ObjectId
 import json
 from json import dumps
 import pandas as pd
 
 
 # Create your views here.
-def home(request,id,comment):
+def home(request,commentId):
     print("hello")
-    print(comment)
-    # df = pd.DataFrame({'genre':'','reviews':comment},index =[0])
-    data_manager = DataManager(comment)
+    print(commentId)
+    #get comment from database using commentId
+    db = demo()
+    collection = db['comments']
+    comment = collection.find_one({"_id":ObjectId(commentId)})
+    print(comment.get('comment'))
+    data_manager = DataManager(comment.get('comment'))
     data_manager.initiate_cleaning()
     sentiment = data_manager.get_result()
     result = json.dumps(sentiment)
